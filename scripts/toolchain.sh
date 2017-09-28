b69363539b3ebb10dc93410c5eff852110d7aada
Ivan Mikushin <imikushin@vmware.com>
2017-09-28 13:40:29 -0700

    Prepare to check in vendored libraries
    Before checking in vendored code, run `prune`.
    `prune` is installed by `scripts/toolchain.sh`.
    
    Updates to ./vendor should ideally be separate commits.

--- scripts/toolchain.sh
+++ scripts/toolchain.sh
@@ -5,6 +5,7 @@ set -x
 go get -u github.com/jteeuwen/go-bindata/...
 go get -u github.com/alecthomas/gometalinter
 go get -u github.com/vektra/mockery/.../
+go get -u github.com/imikushin/prune
 
 # install all the linters
 gometalinter --install --update
