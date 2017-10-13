3f464af1ca2cb3f1ce2776fabb7529ae154d20d4
Karol Stepniewski <kstepniewski@vmware.com>
2017-10-13 14:02:06 -0700

    CI for k8s
    adds new workflow to test full stack:
    * builds docker images for VMware serverless services
    * pushes them to internal repo with CI tag
    * runs a gitlab runner inside k8s cluster
    * deploys openfaas/openwhisk using helm
    * deploys VMware serverless using helm
    * cleans up
    
    TODO:
    add actual tests that verify services are running correctly

--- scripts/images.sh
+++ scripts/images.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-: ${REPO:="serverless-docker-local.artifactory.eng.vmware.com"}
-: ${VALUES_PATH:="values.yaml"}
+: ${DOCKER_REGISTRY:="serverless-docker-local.artifactory.eng.vmware.com"}
 
 PACKAGE=${1}
 BUILD=${2}
 TAG=dev-${BUILD}
-if [ ! -z $CI ]; then
-    TAG=ci-${CI_JOB_ID}
+
+if [ -n $CI ]; then
+    TAG=$IMAGE_TAG
 fi
 
-image=${REPO}/${PACKAGE}:${TAG}
+image=${DOCKER_REGISTRY}/${PACKAGE}:${TAG}
 echo $image
 
 docker build -t $image -f images/${PACKAGE}/Dockerfile .
