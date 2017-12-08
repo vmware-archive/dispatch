8158e2b912409b172c57fcf839427324afbc514e
Ivan Mikushin <imikushin@vmware.com>
2017-12-08 12:46:45 -0800

    Fix the panic getting run.Logs()

--- pkg/functions/context.go
+++ pkg/functions/context.go
@@ -7,17 +7,29 @@ package functions
 import (
 	"bufio"
 	"io"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/vmware/dispatch/pkg/trace"
 )
 
 func (ctx Context) Logs() []string {
-	return ctx["logs"].([]string)
+	defer trace.Tracef("")()
+
+	log.Debugf(`Logs from ctx["logs"]: %v`, ctx["logs"])
+	logs, _ := ctx["logs"].([]string)
+	return logs
 }
 
 func (ctx Context) SetLogs(reader io.Reader) {
+	defer trace.Tracef("")()
+
 	ctx["logs"] = getLogs(reader)
 }
 
 func getLogs(reader io.Reader) []string {
+	defer trace.Tracef("")()
+
 	scanner := bufio.NewScanner(reader)
 	var logs []string
 	for scanner.Scan() {
--- pkg/functions/context_test.go
+++ pkg/functions/context_test.go
@@ -16,3 +16,16 @@ func TestContext_Logs(t *testing.T) {
 	ctx.SetLogs(bytes.NewReader([]byte("foo\nbar\n")))
 	assert.Equal(t, []string{"foo", "bar"}, ctx.Logs())
 }
+
+func TestContext_LogsBS(t *testing.T) {
+	ctx := Context{}
+	ctx["logs"] = &struct {
+		bs string
+	}{}
+	assert.Len(t, ctx.Logs(), 0)
+}
+
+func TestContext_LogsNil(t *testing.T) {
+	ctx := Context{}
+	assert.Len(t, ctx.Logs(), 0)
+}
