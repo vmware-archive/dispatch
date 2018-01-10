adc2fe824e058e3e7a86f9dc21085b0b3a563755
Karol Stepniewski <kstepniewski@vmware.com>
2018-01-10 14:27:06 -0800

    update examples

--- examples/python3/hello.py
+++ examples/python3/hello.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+#######################################################################
+## Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+## SPDX-License-Identifier: Apache-2.0
+#######################################################################
+"""
+Example function "Hello World"
+
+** REQUIREMENTS **
+
+* image
+dispatch create base-image python3 vmware/dispatch-openfaas-python-base:0.0.5-dev1 --language python3 --public
+dispatch create image python3 python3
+
+Create a function:
+dispatch create function python3 hello-python examples/python3/hello.py
+
+Execute it:
+dispatch exec hello-python --wait --input='{"name": "Jon", "place": "Winterfell"}'
+
+"""
+
 def handle(ctx, payload):
     name = payload.get("name", "Noone")  
     place = payload.get("place", "Nowhere")
