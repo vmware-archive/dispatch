31076e12aeb8be4253c6864d4e50f76d0bd79211
Karol Stepniewski <kstepniewski@vmware.com>
2018-03-27 14:48:50 -0700

    Reduce unit tests CI job chatiness
    Due to issue https://github.com/concourse/concourse/issues/1543,
    Concourse UI becomes unresponsive when job has a lot of runs, and runs
    have a lot of output in each of them. The remediate some of it,
    This patch reduces the amount of output produced by unit tests jobs.
    **Changes:**
    * Fix tput `unknown terminal` issue by using `TERM=linux` in photon.
    * Use `quiet` mode by default in swagger generate
    * Redirect the stdout of multiple jobs to /dev/null. Errors are still
    printed.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,6 +3,7 @@ set -e -o pipefail
 
 : ${WORKDIR:="/root/go/src/github.com/vmware/dispatch"}
 : ${CI_IMAGE:="vmware/dispatch-golang-ci:1.10"}
+: ${QUIET:="-q"}
 
 echo Using image ${CI_IMAGE}
 
@@ -10,12 +11,13 @@ PACKAGE=${1}
 APP=${2}
 SWAGGER=${3}
 
-mkdir -p ./pkg/$PACKAGE/gen
-if [ -z $CI ]; then
-    docker run --rm -v `pwd`:$WORKDIR $CI_IMAGE bash -c "cd $WORKDIR && swagger generate server -A $APP -t ./pkg/$PACKAGE/gen -f ./swagger/$SWAGGER --exclude-main"
-    docker run --rm -v `pwd`:$WORKDIR $CI_IMAGE bash -c "cd $WORKDIR && swagger generate client -A $APP -t ./pkg/$PACKAGE/gen -f ./swagger/$SWAGGER"
+
+mkdir -p ./pkg/${PACKAGE}/gen
+if [[ -z ${CI} ]]; then
+    docker run --rm -v `pwd`:${WORKDIR} ${CI_IMAGE} bash -c "cd ${WORKDIR} && swagger generate server ${QUIET} -A ${APP} -t ./pkg/${PACKAGE}/gen -f ./swagger/${SWAGGER} --exclude-main"
+    docker run --rm -v `pwd`:${WORKDIR} ${CI_IMAGE} bash -c "cd ${WORKDIR} && swagger generate client ${QUIET} -A ${APP} -t ./pkg/${PACKAGE}/gen -f ./swagger/${SWAGGER}"
 else
-    echo "CI is set $CI"
-    swagger generate server -A $APP -t ./pkg/$PACKAGE/gen -f ./swagger/$SWAGGER --exclude-main
-    swagger generate client -A $APP -t ./pkg/$PACKAGE/gen -f ./swagger/$SWAGGER
+    echo "CI is set to ${CI}"
+    swagger generate server ${QUIET} -A ${APP} -t ./pkg/${PACKAGE}/gen -f ./swagger/${SWAGGER} --exclude-main
+    swagger generate client ${QUIET} -A ${APP} -t ./pkg/${PACKAGE}/gen -f ./swagger/${SWAGGER}
 fi
