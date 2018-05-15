51985eb7a7266f216d5489825be32343c5be0667
Karol Stepniewski <kstepniewski@vmware.com>
2018-05-15 13:57:39 -0700

    Use command string in generate script to ensure consistency
    In previous patch the generate-models.sh had an incorrect setting for
    one of the variables (WORK_DIR). It wasn't caught in CI, because
    CI used different command (direct swagger call instead of docker).
    This patch fixes the WORK_DIR for generate-models, and also extracts
    the command to a shared so variable, so that CI and local execute the
    same one.

--- scripts/generate-models.sh
+++ scripts/generate-models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -o pipefail
 
-: ${WORKDIR:="-o"}
+: ${WORKDIR:="/root/go/src/github.com/vmware/dispatch"}
 : ${CI_IMAGE:="vmware/dispatch-golang-ci:1.10-20180512"}
 : ${QUIET:="-q"}
 : ${PACKAGE:="github.com/vmware/dispatch/pkg/api/v1"}
@@ -10,9 +10,13 @@ echo Using image ${CI_IMAGE}
 
 TARGET=${1}
 
+GENERATE_COMMAND="pushd ${WORKDIR} && \
+         CGO_ENABLED=0 swagger generate spec ${QUIET} -o ${TARGET} -b ${PACKAGE} -m && \
+         popd"
+
 if [[ -z ${CI} ]]; then
-docker run --rm -v `pwd`:${WORKDIR} ${CI_IMAGE} bash -c "cd ${WORKDIR} && CGO_ENABLED=0 swagger generate spec ${QUIET} -o ${TARGET} -b ${PACKAGE} -m"
+docker run --rm -v `pwd`:${WORKDIR} ${CI_IMAGE} bash -c "${GENERATE_COMMAND}"
 else
     echo "CI is set to ${CI}"
-    CGO_ENABLED=0 swagger generate spec ${QUIET} -o ${TARGET} -b ${PACKAGE} -m
+    bash -c "${GENERATE_COMMAND}"
 fi
