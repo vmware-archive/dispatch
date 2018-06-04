8846e317d8ced47bdbd6260f9a64a60c8188cbaa
Sabari Kumar Murugesan <sabari.bits@gmail.com>
2018-06-04 16:39:21 -0700

    Add Org API/Model/Swagger Defs
    Adding all the boilerplate stuff for creating an Org.

---
+++ pkg/api/v1/organization.go
@@ -0,0 +1,149 @@
+///////////////////////////////////////////////////////////////////////
+// Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+///////////////////////////////////////////////////////////////////////
+
+package v1
+
+import (
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/swag"
+	"github.com/go-openapi/validate"
+)
+
+// NO TESTS
+
+// Organization organization
+// swagger:model Organization
+type Organization struct {
+
+	// created time
+	// Read Only: true
+	CreatedTime int64 `json:"createdTime,omitempty"`
+
+	// id
+	ID strfmt.UUID `json:"id,omitempty"`
+
+	// kind
+	// Read Only: true
+	// Pattern: ^[\w\d\-]+$
+	Kind string `json:"kind,omitempty"`
+
+	// modified time
+	// Read Only: true
+	ModifiedTime int64 `json:"modifiedTime,omitempty"`
+
+	// name
+	// Required: true
+	// Pattern: ^[\w\d\-]+$
+	Name *string `json:"name"`
+
+	// status
+	// Read Only: true
+	Status Status `json:"status,omitempty"`
+}
+
+// Validate validates this organization
+func (m *Organization) Validate(formats strfmt.Registry) error {
+	var res []error
+
+	if err := m.validateID(formats); err != nil {
+		// prop
+		res = append(res, err)
+	}
+
+	if err := m.validateKind(formats); err != nil {
+		// prop
+		res = append(res, err)
+	}
+
+	if err := m.validateName(formats); err != nil {
+		// prop
+		res = append(res, err)
+	}
+
+	if err := m.validateStatus(formats); err != nil {
+		// prop
+		res = append(res, err)
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}
+
+func (m *Organization) validateID(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.ID) { // not required
+		return nil
+	}
+
+	if err := validate.FormatOf("id", "body", "uuid", m.ID.String(), formats); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Organization) validateKind(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.Kind) { // not required
+		return nil
+	}
+
+	if err := validate.Pattern("kind", "body", string(m.Kind), `^[\w\d\-]+$`); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Organization) validateName(formats strfmt.Registry) error {
+
+	if err := validate.Required("name", "body", m.Name); err != nil {
+		return err
+	}
+
+	if err := validate.Pattern("name", "body", string(*m.Name), `^[\w\d\-]+$`); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Organization) validateStatus(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.Status) { // not required
+		return nil
+	}
+
+	if err := m.Status.Validate(formats); err != nil {
+		if ve, ok := err.(*errors.Validation); ok {
+			return ve.ValidateName("status")
+		}
+		return err
+	}
+
+	return nil
+}
+
+// MarshalBinary interface implementation
+func (m *Organization) MarshalBinary() ([]byte, error) {
+	if m == nil {
+		return nil, nil
+	}
+	return swag.WriteJSON(m)
+}
+
+// UnmarshalBinary interface implementation
+func (m *Organization) UnmarshalBinary(b []byte) error {
+	var res Organization
+	if err := swag.ReadJSON(b, &res); err != nil {
+		return err
+	}
+	*m = res
+	return nil
+}
