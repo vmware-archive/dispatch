39c0a41dda7e6db246da4593e07b136fc63fb41b
tenczar <nicholas.tenczar@gmail.com>
2018-06-04 17:45:54 -0700

    [git/validate] Fix swagger validation in pre-commit hook (#490)
    Adds main api package level doc.go that `swagger generate` uses to populate the info field in models.json. Allows models.json to pass `swagger validate`.

---
+++ pkg/api/v1/doc.go
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////////////////////
+// Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+///////////////////////////////////////////////////////////////////////
+
+/*
+Package v1 Dispatch API.
+
+The API for Dispatch framework instances.
+
+    Version: 1.0.0
+    Contact: <dispatch@vmware.com>
+
+swagger:meta
+*/
+package v1
+
+// NO TEST
