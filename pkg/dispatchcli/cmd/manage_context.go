1c5cc72ff7f5d41b709fa6aa82315d43741e11a3
zimengyang <zmyang2011@gmail.com>
2018-08-02 14:59:45 -0700

    Add set,delete,current for manage context

--- pkg/dispatchcli/cmd/manage_context.go
+++ pkg/dispatchcli/cmd/manage_context.go
@@ -21,48 +21,81 @@ import (
 )
 
 var (
-	manageContextLong = i18n.T(`Manage configuration context.`)
+	manageContextLong    = i18n.T(`Manage configuration context.`)
+	manageContextExample = i18n.T(`
+# list all contexts:
+$ dispatch manage context`)
 
-	// TODO: add examples
-	manageContextExample = i18n.T(``)
-	currentContext       = i18n.T(``)
+	currentContextLong = i18n.T(`Show current configuration context.`)
+
+	setContextLong    = i18n.T(`Set configuration context.`)
+	setContextExample = i18n.T(`
+# set current context to localhost:
+$ dispatch manage context set localhost
+Set context to localhost`)
+
+	deleteContextLong    = i18n.T(`Delete configuration context.`)
+	deleteContextExample = i18n.T(`
+# delete context localhost:
+$ dispatch manage context delete localhost
+Deleted context localhost`)
 )
 
 // NewCmdManageContext handles configuration context operations
 func NewCmdManageContext(out io.Writer, errOut io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "context [--set CONTEXT]",
+		Use:     "context",
 		Short:   i18n.T("Manage context"),
 		Long:    manageContextLong,
 		Example: manageContextExample,
-		Args:    cobra.ExactArgs(0),
 		Aliases: []string{"app"},
-		Run: func(cmd *cobra.Command, args []string) {
-			err := manageContext(out, errOut, cmd, args)
-			CheckErr(err)
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return formatContextOutput(out)
 		},
 	}
 
-	cmd.Flags().StringVarP(&currentContext, "set", "s", "", "Set current context")
+	cmd.AddCommand(NewCmdManageContextCurrent(out, errOut))
+	cmd.AddCommand(NewCmdManageContextSet(out, errOut))
+	cmd.AddCommand(NewCmdManageContextDelete(out, errOut))
+
 	return cmd
 }
 
-func manageContext(out, errOut io.Writer, cmd *cobra.Command, args []string) error {
-	if currentContext != "" {
-		if _, ok := cmdConfig.Contexts[currentContext]; !ok {
-			return errors.Errorf("No such context %s", currentContext)
-		}
-		cmdConfig.Current = currentContext
-		b, _ := json.MarshalIndent(cmdConfig, "", "    ")
-		path := viper.ConfigFileUsed()
-		err := ioutil.WriteFile(path, b, 0644)
-		if err != nil {
-			return errors.Errorf("Failed to write config file %s", path)
-		}
-		fmt.Fprintf(out, "Set context to %s\n", currentContext)
-		return nil
+func setContext(out, errOut io.Writer, cmd *cobra.Command, args []string) error {
+	currentContext := args[0]
+	if _, ok := cmdConfig.Contexts[currentContext]; !ok {
+		return errors.Errorf("No such context %s", currentContext)
 	}
-	return formatContextOutput(out)
+	cmdConfig.Current = currentContext
+	b, _ := json.MarshalIndent(cmdConfig, "", "    ")
+	path := viper.ConfigFileUsed()
+	err := ioutil.WriteFile(path, b, 0644)
+	if err != nil {
+		return errors.Errorf("Failed to write config file %s", path)
+	}
+	fmt.Fprintf(out, "Set context to %s\n", currentContext)
+	return nil
+}
+
+func deleteContext(out, errOut io.Writer, cmd *cobra.Command, args []string) error {
+	ctxName := args[0]
+	if _, ok := cmdConfig.Contexts[ctxName]; !ok {
+		return errors.Errorf("No such context %s", ctxName)
+	}
+
+	if cmdConfig.Current == ctxName {
+		return errors.Errorf("Error: trying to delete active context %s, please switch to other context and try again.", ctxName)
+	}
+
+	delete(cmdConfig.Contexts, ctxName)
+	b, _ := json.MarshalIndent(cmdConfig, "", "    ")
+	path := viper.ConfigFileUsed()
+	err := ioutil.WriteFile(path, b, 0644)
+	if err != nil {
+		return errors.Errorf("Failed to write config file %s", path)
+	}
+	fmt.Fprintf(out, "Deleted context %s\n", ctxName)
+	return nil
 }
 
 func formatContextOutput(out io.Writer) error {
@@ -90,6 +123,75 @@ func formatContextOutput(out io.Writer) error {
 	return nil
 }
 
+func formatCurrentContextOutput(out io.Writer) error {
+	if w, err := formatOutput(out, false, cmdConfig); w {
+		return err
+	}
+
+	headers := []string{"Context", "Config"}
+	table := tablewriter.NewWriter(out)
+	table.SetHeader(headers)
+	table.SetBorders(tablewriter.Border{Left: false, Top: false, Right: false, Bottom: false})
+	table.SetCenterSeparator("")
+	table.SetAutoWrapText(false)
+	context := cmdConfig.Current
+	config := cmdConfig.Contexts[context]
+	// Remove the cookie from output
+	config.Cookie = ""
+	configContent, _ := json.MarshalIndent(config, "", "  ")
+	context = fmt.Sprintf("* %s", context)
+	row := []string{context, string(configContent)}
+	table.Append(row)
+	table.Render()
+	return nil
+}
+
 func formatContextName(name string) string {
 	return strings.ToLower(strings.Replace(name, ".", "-", -1))
+}
+
+// NewCmdManageContextCurrent show current context
+func NewCmdManageContextCurrent(out io.Writer, errOut io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "current",
+		Short: i18n.T("Show current context"),
+		Long:  currentContextLong,
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return formatCurrentContextOutput(out)
+		},
+	}
+	return cmd
+}
+
+// NewCmdManageContextSet handles set context operations
+func NewCmdManageContextSet(out io.Writer, errOut io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "set CONTEXT",
+		Short:   i18n.T("Set context"),
+		Long:    setContextLong,
+		Example: setContextExample,
+		Args:    cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			err := setContext(out, errOut, cmd, args)
+			CheckErr(err)
+		},
+	}
+	return cmd
+}
+
+// NewCmdManageContextDelete handles delete context operations
+func NewCmdManageContextDelete(out io.Writer, errOut io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "delete CONTEXT",
+		Short:   i18n.T("Delete context"),
+		Long:    deleteContextLong,
+		Example: deleteContextExample,
+		Args:    cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			err := deleteContext(out, errOut, cmd, args)
+			CheckErr(err)
+		},
+	}
+	return cmd
 }
