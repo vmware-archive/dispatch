fab5894509cd43faa987701a5ed2d6114239fe83
Nicolaas Kaashoek <nick.kaashoek@gmail.com>
2018-08-16 15:38:54 -0700

    Use istio gateway instead of kong

--- pkg/dispatchserver/apis.go
+++ pkg/dispatchserver/apis.go
@@ -14,7 +14,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/vmware/dispatch/pkg/api-manager"
 	"github.com/vmware/dispatch/pkg/api-manager/gateway"
-	"github.com/vmware/dispatch/pkg/api-manager/gateway/kong"
+	"github.com/vmware/dispatch/pkg/api-manager/gateway/istio"
 	"github.com/vmware/dispatch/pkg/api-manager/gen/restapi"
 	"github.com/vmware/dispatch/pkg/api-manager/gen/restapi/operations"
 	"github.com/vmware/dispatch/pkg/dispatchcli/i18n"
@@ -47,10 +47,7 @@ func NewCmdAPIs(out io.Writer, config *serverConfig) *cobra.Command {
 func runAPIs(config *serverConfig) {
 	store := entityStore(config)
 
-	gw, err := kong.NewClient(&kong.Config{
-		Host:     config.APIs.GatewayHost,
-		Upstream: config.FunctionManager,
-	})
+	gw, err := istio.NewClient()
 	if err != nil {
 		log.Fatalf("Error creating an api gateway client: %v", err)
 	}
