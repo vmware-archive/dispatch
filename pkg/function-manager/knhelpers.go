263047699395a8025fe1880b8cafb40162e3e9e5
Ivan Mikushin <imikushin@vmware.com>
2018-08-17 14:18:30 -0700

    WIP: create function: first shot

--- pkg/function-manager/knhelpers.go
+++ pkg/function-manager/knhelpers.go
@@ -6,15 +6,46 @@
 package functionmanager
 
 import (
+	"strings"
+
 	kntypes "github.com/knative/serving/pkg/apis/serving/v1alpha1"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 
 	dapi "github.com/vmware/dispatch/pkg/api/v1"
 	"github.com/vmware/dispatch/pkg/utils/knaming"
 )
 
 func ToKnService(function *dapi.Function) *kntypes.Service {
+	probe := &v1.Probe{
+		Handler: v1.Handler{
+			HTTPGet: &v1.HTTPGetAction{
+				Path: "/healthz",
+				Port: intstr.FromInt(8080),
+			},
+		},
+	}
 	return &kntypes.Service{
-		ObjectMeta: knaming.ToObjectMeta(function.Meta, function),
+		ObjectMeta: *knaming.ToObjectMeta(function.Meta, function),
+		Spec: kntypes.ServiceSpec{
+			RunLatest: &kntypes.RunLatestType{
+				Configuration: kntypes.ConfigurationSpec{
+					RevisionTemplate: kntypes.RevisionTemplateSpec{
+						Spec: kntypes.RevisionSpec{
+							Container: v1.Container{
+								Image: function.FunctionImageURL,
+								Env: []v1.EnvVar{
+									{Name: "SERVERS", Value: "1"},
+									{Name: "SECRETS", Value: strings.Join(function.Secrets, ",")},
+								},
+								LivenessProbe:  probe,
+								ReadinessProbe: probe,
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 }
 
