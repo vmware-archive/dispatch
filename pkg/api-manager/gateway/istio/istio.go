4731dee51e8adbdd690c2126c6534b038d5edf7c
Nicolaas Kaashoek <nick.kaashoek@gmail.com>
2018-08-17 09:45:03 -0700

    Fix docker registry, istio skeleton

--- pkg/api-manager/gateway/istio/istio.go
+++ pkg/api-manager/gateway/istio/istio.go
@@ -2,41 +2,40 @@ package istio
 
 import (
 	"context"
-	"flag"
-	"fmt"
-	"log"
 
 	"istio.io/istio/pilot/pkg/config/kube/crd"
-	// "istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pilot/pkg/model"
 
 	ewrapper "github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/vmware/dispatch/pkg/api-manager/gateway"
 )
 
 func NewClient() (*Client, error) {
+	log.Infoln("Trying to create istio client")
 	crdClient, err := crd.NewClient("", "", []model.ProtoSchema{model.VirtualService}, "")
 	if err != nil {
 		return nil, err
 	}
 	return &Client{
 		istioClient: crdClient,
-	}
+	}, nil
 }
 
 type Client struct {
 	istioClient *crd.Client
 }
 
-func (c *Client) AddAPI(ctx context.Context, api *API) (*API, error) {
+func (c *Client) AddAPI(ctx context.Context, api *gateway.API) (*gateway.API, error) {
 	return nil, ewrapper.Errorf("Not implemented yet dummy")
 }
-func (c *Client) GetAPI(ctx context.Context, name string) (*API, error) {
+func (c *Client) GetAPI(ctx context.Context, name string) (*gateway.API, error) {
 	return nil, ewrapper.Errorf("Not implemented yet dummy")
 }
-func (c *Client) UpdateAPI(ctx context.Context, name string, api *API) (*API, error) {
+func (c *Client) UpdateAPI(ctx context.Context, name string, api *gateway.API) (*gateway.API, error) {
 	return nil, ewrapper.Errorf("Not implemented yet dummy")
 }
-func (c *Client) DeleteAPI(ctx context.Context, api *API) error {
-	return nil, ewrapper.Errorf("Not implemented yet dummy")
+func (c *Client) DeleteAPI(ctx context.Context, api *gateway.API) error {
+	return ewrapper.Errorf("Not implemented yet dummy")
 }
