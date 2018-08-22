bd520558340060be1b7d11ee2abb6ec47b7f1246
Nicolaas Kaashoek <nick.kaashoek@gmail.com>
2018-08-22 10:17:21 -0700

    Istiohandlers

---
+++ pkg/api-manager/istio/istio.go
@@ -0,0 +1,99 @@
+package istio
+
+import (
+	"context"
+	"strings"
+
+	"istio.io/istio/pilot/pkg/config/kube/crd"
+	"istio.io/istio/pilot/pkg/model"
+
+	ewrapper "github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/vmware/dispatch/pkg/api-manager/gateway"
+	"github.com/vmware/dispatch/pkg/trace"
+)
+
+const (
+	// InternalGateway represents the gateway that we should redirect to
+	// Should probably come from a config file
+	InternalGateway = "knative-ingressgateway.istio-system.svc.cluster.local"
+)
+
+func NewClient() (*Client, error) {
+	log.Infoln("Trying to create istio client")
+	crdClient, err := crd.NewClient("", "", []model.ProtoSchema{model.VirtualService}, "")
+	if err != nil {
+		return nil, err
+	}
+	return &Client{
+		istioClient: crdClient,
+	}, nil
+}
+
+type Client struct {
+	istioClient *crd.Client
+}
+
+func (c *Client) AddAPI(ctx context.Context, specs, name string) error {
+	span, ctx := trace.Trace(ctx, "")
+	defer span.Finish()
+
+	vs := model.VirtualService
+	spec, err := vs.FromYAML(specs)
+	if err != nil {
+		return ewrapper.Wrapf(err, "Unable to build virtual service from spec string")
+	}
+
+	cfg := model.Config{
+		model.ConfigMeta{
+			Type:    "virtual-service",
+			Group:   "networking.istio.io",
+			Version: "v1alpha3",
+			Name:    name,
+		},
+		spec,
+	}
+
+	result, err := c.istioClient.Create(cfg)
+	if err != nil {
+		return ewrapper.Wrapf(err, "Unable to create virtual service")
+	}
+	log.Debugf("Created object with resourceVersion: %v", result)
+
+	return nil
+}
+
+func (c *Client) GetAPI(ctx context.Context, name string) (string, error) {
+	span, ctx := trace.Trace(ctx, "")
+	defer span.Finish()
+
+	cfg, found := c.istioClient.Get("virtual-service", name, "default")
+	if !found {
+		return "", ewrapper.Errorf("Istio couldn't located the api %s you requested", name)
+	}
+	str, err := model.ToYAML(cfg.Spec)
+	if err != nil {
+		return "", ewrapper.Wrapf(err, "Unable to turn cfg spec into string")
+	}
+	log.Infof("Grabbed istio config: %s", str)
+	return str, nil
+}
+
+func (c *Client) UpdateAPI(ctx context.Context, name string, api *gateway.API) error {
+	span, ctx := trace.Trace(ctx, "")
+	defer span.Finish()
+
+	return c.AddAPI(ctx, "", name)
+}
+
+func (c *Client) DeleteAPI(ctx context.Context, api *gateway.API) error {
+	span, ctx := trace.Trace(ctx, "")
+	defer span.Finish()
+
+	err := c.istioClient.Delete("virtual-service", api.Name, "default")
+	if err != nil && !strings.HasSuffix(err.Error(), "not found") {
+		return ewrapper.Wrapf(err, "Unable to delete api %+v", api)
+	}
+	return nil
+}
---
+++ pkg/api-manager/istiohandler.go
@@ -0,0 +1,171 @@
+package apimanager
+
+import (
+	"fmt"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/go-openapi/runtime/middleware"
+
+	"github.com/ghodss/yaml"
+	"github.com/vmware/dispatch/pkg/api-manager/gen/restapi/operations/endpoint"
+	"github.com/vmware/dispatch/pkg/api-manager/istio"
+	"github.com/vmware/dispatch/pkg/api/v1"
+	"github.com/vmware/dispatch/pkg/trace"
+)
+
+const (
+	// InternalGateway represents the gateway that we should redirect to
+	// Should probably come from a config file
+	InternalGateway = "knative-ingressgateway.istio-system.svc.cluster.local"
+)
+
+func makeMethodRegex(methods []string) string {
+	return strings.Join(methods, "|") + "/g"
+}
+
+func makeMethodsFromRegex(regex string) []string {
+	split := strings.Split(regex, "|")
+	return split[0 : len(split)-1]
+}
+
+func apiModelOntoIstioEntity(m *v1.API) VirtualService {
+	cors := CorsPolicy{}
+	if m.Cors {
+		cors.AllowMethods = m.Methods
+	}
+	var routes []HttpRoute
+	for _, prefix := range m.Uris {
+		match := HttpMatch{
+			URI:    URI{Prefix: prefix},
+			Method: MethodMatch{Regex: makeMethodRegex(m.Methods)},
+		}
+		route := HttpRoute{
+			Match: []HttpMatch{match},
+			Route: []DestWeight{DestWeight{
+				Destination: RouteDest{
+					Host: InternalGateway,
+				},
+				Weight: 100,
+			}},
+			Rewrite: HttpRewrite{
+				Authority: fmt.Sprintf("%v.default.example.com", m.Function),
+			},
+			CORS: cors,
+		}
+		routes = append(routes, route)
+	}
+
+	hosts := m.Hosts
+	if len(m.Hosts) == 0 {
+		hosts = append(hosts, "*")
+	}
+
+	return VirtualService{
+		Name:     *m.Name,
+		Function: *m.Function,
+		Hosts:    hosts,
+		Gateways: []string{"knative-shared-gateway.knative-serving.svc.cluster.local"},
+		HTTP:     routes,
+	}
+}
+
+func corsEnabled(cors CorsPolicy) bool {
+	if len(cors.AllowOrigin) != 0 {
+		return true
+	} else if len(cors.AllowMethods) != 0 {
+		return true
+	} else if len(cors.AllowHeaders) != 0 {
+		return true
+	}
+	return false
+}
+
+func istioEntityToModel(vs VirtualService) *v1.API {
+	var uris []string
+	var methods []string
+	var function string
+	var cors bool
+	hosts := vs.Hosts
+	for _, route := range vs.HTTP {
+		uris = append(uris, route.Match[0].URI.Prefix)
+		methods = makeMethodsFromRegex(route.Match[0].Method.Regex)
+		function = strings.Split(route.Rewrite.Authority, ".")[0]
+		if corsEnabled(route.CORS) {
+			cors = true
+		}
+	}
+	return &v1.API{
+		Enabled:   true,
+		Methods:   methods,
+		Name:      &vs.Name,
+		Uris:      uris,
+		Hosts:     hosts,
+		Function:  &function,
+		Protocols: []string{"http"},
+		Cors:      cors,
+	}
+}
+
+type IstioHandlers struct {
+	client *istio.Client
+}
+
+func NewIstioHandlers(client *istio.Client) *IstioHandlers {
+	return &IstioHandlers{
+		client: client,
+	}
+}
+
+func (cl *IstioHandlers) AddAPI(params endpoint.AddAPIParams, principal interface{}) middleware.Responder {
+	span, ctx := trace.Trace(params.HTTPRequest.Context(), "")
+	defer span.Finish()
+
+	log.Infof("Trying to add istio api: %+v", params)
+
+	api := apiModelOntoIstioEntity(params.Body)
+	virtualServiceSpec, err := yaml.Marshal(api)
+	if err != nil {
+		log.Errorf("Failed to marshal virtualservicspec: %+v", api)
+	}
+	err = cl.client.AddAPI(ctx, string(virtualServiceSpec), api.Name)
+	if err != nil {
+		log.Errorf("Istio failed to create the api: %v", err)
+	}
+	m := istioEntityToModel(api)
+	log.Infof("Added api: %+v", m)
+	return endpoint.NewAddAPIOK().WithPayload(m)
+}
+
+func (cl *IstioHandlers) GetAPI(params endpoint.GetAPIParams, principal interface{}) middleware.Responder {
+	span, ctx := trace.Trace(params.HTTPRequest.Context(), "")
+	defer span.Finish()
+
+	log.Infof("Trying to get an istio api: %+v", params)
+	name := params.API
+	_, err := cl.client.GetAPI(ctx, name)
+	if err != nil {
+		log.Errorf("Couldn't get api %v: %v", name, err)
+	}
+
+	m := &v1.API{
+		Name: &name,
+	}
+	return endpoint.NewGetAPIOK().WithPayload(m)
+}
+
+func (cl *IstioHandlers) GetAPIs(params endpoint.GetApisParams, principal interface{}) middleware.Responder {
+	log.Errorf("Not ready to get multiple apis")
+	return nil
+}
+
+func (cl *IstioHandlers) UpdateAPI(params endpoint.UpdateAPIParams, principal interface{}) middleware.Responder {
+	log.Errorf("Not ready to update api")
+	return nil
+}
+
+func (cl *IstioHandlers) DeleteAPI(params endpoint.DeleteAPIParams, principal interface{}) middleware.Responder {
+	log.Errorf("Not ready to delete api")
+	return nil
+}
