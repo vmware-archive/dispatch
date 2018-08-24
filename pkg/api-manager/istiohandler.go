4ab746d23a311195747f8271bc7bce3e0c33d8eb
Nicolaas Kaashoek <nick.kaashoek@gmail.com>
2018-08-24 15:49:52 -0700

    Filled in the rest

--- pkg/api-manager/istiohandler.go
+++ pkg/api-manager/istiohandler.go
@@ -109,12 +109,126 @@ func apiModelOntoIstioEntity(m *v1.API) v1alpha3.VirtualServiceSpec {
 		}
 		routes = append(routes, route)
 	}
-
+	log.Infof("Routes: %+v", routes)
 	result.Http = routes
 	return result
 }
 
+func istioEntityOntoAPIModel(vs v1alpha3.VirtualServiceSpec) *v1.API {
+	var result v1.API
+	var uris []string
+	var methods []string
+	var function string
+	hosts := vs.Hosts
+	for _, route := range vs.Http {
+		uris = append(uris, route.Match[0].Uri.Prefix)
+		methods = makeMethodsFromRegex(route.Match[0].Method.Regex)
+		function = strings.Split(route.Rewrite.Authority, ".")[0]
+	}
+	result.Enabled = true
+	result.Methods = methods
+	result.Uris = uris
+	result.Hosts = hosts
+	result.Function = &function
+	result.Protocols = []string{"Http"}
+	return &result
+}
+
 func (cl *IstioHandlers) AddAPI(params endpoint.AddAPIParams, principal interface{}) middleware.Responder {
+	span, _ := trace.Trace(params.HTTPRequest.Context(), "")
+	defer span.Finish()
+
+	log.Infof("Trying to add istio api: %+v", params)
+
+	ns := params.XDispatchOrg
+
+	specs := apiModelOntoIstioEntity(params.Body)
+
+	virtualService := v1alpha3.VirtualService{
+		metav1.TypeMeta{
+			Kind:       "VirtualService",
+			APIVersion: "networking.istio.io/v1alpha3",
+		},
+		metav1.ObjectMeta{
+			Name:      *params.Body.Name,
+			Namespace: ns,
+			Labels: map[string]string{
+				"Organization": ns,
+			},
+		},
+		specs,
+	}
+
+	log.Infof("Trying to create VirtualService: %+v", virtualService)
+	newVirtualService, err := cl.knClient.NetworkingV1alpha3().VirtualServices(ns).Create(&virtualService)
+	if err != nil {
+		log.Errorf("Couldn't Create API: %v", err)
+		return endpoint.NewAddAPIDefault(http.StatusInternalServerError).WithPayload(
+			&v1.Error{
+				Code:    http.StatusNotFound,
+				Message: swag.String("Failed to create api"),
+			})
+	}
+	log.Infof("Created Virtual Service: %+v", newVirtualService)
+	return endpoint.NewAddAPIOK().WithPayload(params.Body)
+}
+
+func (cl *IstioHandlers) GetAPI(params endpoint.GetAPIParams, principal interface{}) middleware.Responder {
+	span, _ := trace.Trace(params.HTTPRequest.Context(), "")
+	defer span.Finish()
+
+	ns := params.XDispatchOrg
+	log.Infof("Trying to get an istio api: %+v", params)
+	result, err := cl.knClient.NetworkingV1alpha3().VirtualServices(ns).Get(params.API, metav1.GetOptions{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "VirtualService",
+			APIVersion: "networking.istio.io/v1alpha3",
+		},
+	})
+	if err != nil {
+		log.Infof("Couldn't find api: %v", err)
+		return endpoint.NewGetAPIDefault(http.StatusInternalServerError).WithPayload(
+			&v1.Error{
+				Code:    http.StatusNotFound,
+				Message: swag.String("Failed to find"),
+			})
+	}
+	converted := istioEntityOntoAPIModel(result.Spec)
+	converted.Name = &result.Name
+	return endpoint.NewGetAPIOK().WithPayload(converted)
+}
+
+func (cl *IstioHandlers) GetAPIs(params endpoint.GetApisParams, principal interface{}) middleware.Responder {
+	span, _ := trace.Trace(params.HTTPRequest.Context(), "")
+	defer span.Finish()
+
+	ns := params.XDispatchOrg
+	log.Infof("Getting apis: %+v", params)
+	allVs, err := cl.knClient.NetworkingV1alpha3().VirtualServices(ns).List(metav1.ListOptions{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "VirtualService",
+			APIVersion: "networking.istio.io/v1alpha3",
+		},
+		LabelSelector: "Organization",
+	})
+	if err != nil {
+		log.Infof("Couldn't find api: %v", err)
+		return endpoint.NewGetApisDefault(http.StatusInternalServerError).WithPayload(
+			&v1.Error{
+				Code:    http.StatusNotFound,
+				Message: swag.String("Failed to find"),
+			})
+	}
+	var results []*v1.API
+	for _, vs := range allVs.Items {
+		converted := istioEntityOntoAPIModel(vs.Spec)
+		converted.Name = &vs.Name
+		results = append(results, converted)
+	}
+	return endpoint.NewGetApisOK().WithPayload(results)
+}
+
+func (cl *IstioHandlers) UpdateAPI(params endpoint.UpdateAPIParams, principal interface{}) middleware.Responder {
 	span, _ := trace.Trace(params.HTTPRequest.Context(), "")
 	defer span.Finish()
 
@@ -136,47 +250,41 @@ func (cl *IstioHandlers) AddAPI(params endpoint.AddAPIParams, principal interfac
 		specs,
 	}
 
-	newVirtualService, err := cl.knClient.NetworkingV1alpha3().VirtualServices(ns).Create(&virtualService)
+	log.Infof("Trying to create VirtualService: %+v", virtualService)
+	newVirtualService, err := cl.knClient.NetworkingV1alpha3().VirtualServices(ns).Update(&virtualService)
 	if err != nil {
 		log.Errorf("Couldn't Create API: %v", err)
-		return endpoint.NewAddAPIDefault(http.StatusInternalServerError).WithPayload(
+		return endpoint.NewUpdateAPIDefault(http.StatusInternalServerError).WithPayload(
 			&v1.Error{
 				Code:    http.StatusNotFound,
-				Message: swag.String("Failed to create api"),
+				Message: swag.String("Failed to update api"),
 			})
 	}
 	log.Infof("Created Virtual Service: %+v", newVirtualService)
-	return endpoint.NewAddAPIOK().WithPayload(params.Body)
-}
-
-func (cl *IstioHandlers) GetAPI(params endpoint.GetAPIParams, principal interface{}) middleware.Responder {
-	span, _ := trace.Trace(params.HTTPRequest.Context(), "")
-	defer span.Finish()
-
-	log.Infof("Trying to get an istio api: %+v", params)
-	return nil
-}
-
-func (cl *IstioHandlers) GetAPIs(params endpoint.GetApisParams, principal interface{}) middleware.Responder {
-	span, _ := trace.Trace(params.HTTPRequest.Context(), "")
-	defer span.Finish()
-
-	log.Infof("Getting apis: %+v", params)
-
-	return nil
-}
-
-func (cl *IstioHandlers) UpdateAPI(params endpoint.UpdateAPIParams, principal interface{}) middleware.Responder {
-	span, _ := trace.Trace(params.HTTPRequest.Context(), "")
-	defer span.Finish()
-
-	return nil
+	return endpoint.NewUpdateAPIOK().WithPayload(params.Body)
 }
 
 func (cl *IstioHandlers) DeleteAPI(params endpoint.DeleteAPIParams, principal interface{}) middleware.Responder {
 	span, _ := trace.Trace(params.HTTPRequest.Context(), "")
 	defer span.Finish()
 
+	ns := params.XDispatchOrg
 	log.Infof("Trying to get an istio api: %+v", params)
-	return nil
+	err := cl.knClient.NetworkingV1alpha3().VirtualServices(ns).Delete(params.API, &metav1.DeleteOptions{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "VirtualService",
+			APIVersion: "networking.istio.io/v1alpha3",
+		},
+	})
+	if err != nil {
+		log.Infof("Couldn't delete api: %v", err)
+		return endpoint.NewDeleteAPIDefault(http.StatusInternalServerError).WithPayload(
+			&v1.Error{
+				Code:    http.StatusNotFound,
+				Message: swag.String("Failed to delete api"),
+			})
+	}
+	return endpoint.NewDeleteAPIOK().WithPayload(&v1.API{
+		Name: &params.API,
+	})
 }
