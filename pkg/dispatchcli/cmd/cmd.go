9f2f9de7f7370b116ace6097ab6da3551f21fdc0
Motonori Shindo <motonori@shin.do>
2018-10-16 02:52:48 +0900

    CLI completion for bash added. (#681)
    Signed-off-by: Motonori Shindo <motonori@shin.do>

--- pkg/dispatchcli/cmd/cmd.go
+++ pkg/dispatchcli/cmd/cmd.go
@@ -204,6 +204,7 @@ func NewCLI(in io.Reader, out, errOut io.Writer) *cobra.Command {
 	cmds.AddCommand(NewCmdManage(out, errOut))
 	cmds.AddCommand(NewCmdLog(out, errOut))
 	cmds.AddCommand(NewCmdContext(out, errOut))
+	cmds.AddCommand(NewCmdCompletion(out))
 	return cmds
 }
 
