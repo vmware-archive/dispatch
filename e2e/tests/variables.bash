a4d721467ee50a295f7e7275fdd24d73a8e4ea7b
Zhiming Peng <pzmrzy@gmail.com>
2018-10-18 17:47:21 -0700

    modify image to knative version

--- e2e/tests/variables.bash
+++ e2e/tests/variables.bash
@@ -2,9 +2,9 @@
 
 ### COMMON VARIABLES ###
 : ${DOCKER_REGISTRY:="dispatchframework"}
-: ${BASE_IMAGE_PYTHON3:="python3-base:0.0.13"}
-: ${BASE_IMAGE_NODEJS6:="nodejs-base:0.0.12"}
-: ${BASE_IMAGE_POWERSHELL:="powershell-base:0.0.13"}
-: ${BASE_IMAGE_JAVA:="java-base:0.0.13"}
+: ${BASE_IMAGE_PYTHON3:="python3-base:0.0.14-knative"}
+: ${BASE_IMAGE_NODEJS6:="nodejs-base:0.0.12-knative"}
+: ${BASE_IMAGE_POWERSHELL:="powershell-base:0.0.13-knative"}
+: ${BASE_IMAGE_JAVA:="java-base:0.0.13-knative"}
 : ${IMAGES_YAML:="images.yaml"}
 : ${DISPATCH_ORGANIZATION:=${DISPATCH_ORGANIZATION:-dispatch}}
