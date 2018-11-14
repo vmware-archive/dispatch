7adc5266c8172a1a6149c46ac7627af4af528a78
Berndt Jung <bjung@vmware.com>
2018-11-14 14:22:35 -0800

    update docs structure and purge specs
    Signed-off-by: Berndt Jung <bjung@vmware.com>

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Simple script for updating the docs on release
-# Usage: ./update-docs [release]
+# Usage: ./update-docs branch [release]
 
+BRANCH=${1}
 DOCS_DIR=$(mktemp -d)
 pushd $DOCS_DIR
 mkdir gh-pages
@@ -10,15 +11,15 @@ pushd gh-pages
 git clone git@github.com:vmware/dispatch.git
 cd dispatch && git checkout origin/gh-pages
 popd
-mkdir master
-pushd master
+mkdir ${BRANCH}
+pushd ${BRANCH}
 git clone git@github.com:vmware/dispatch.git
-cd dispatch && git checkout origin/master
+cd dispatch && git checkout origin/${BRANCH}
 popd
-rsync -av --exclude '\.git*' --exclude 'README.md' master/dispatch/docs/ gh-pages/dispatch --delete
+rsync -av --exclude '\.git*' --exclude 'README.md' ${BRANCH}/dispatch/docs/ gh-pages/dispatch --delete
 cd gh-pages/dispatch
 git add .
-git commit -m "updating docs for release ${1}"
+git commit -m "updating docs for release ${2}"
 git checkout -b gh-pages
 git push --force-with-lease origin gh-pages
 popd
