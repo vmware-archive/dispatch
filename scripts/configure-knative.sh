ae27352b87a54d9a092f2235d580790c7ffc3f12
Berndt Jung <bjung@vmware.com>
2018-12-14 16:07:21 -0800

    Add third-party directory for dependencies (#790)
    * Added istio and knative-serving releases in third-party (for vmware cloud PKS)
    * Added instructions for installing on vmware cloud PKS
    * Fixed some scripts
    
    Signed-off-by: Berndt Jung <bjung@vmware.com>

--- scripts/configure-knative.sh
+++ scripts/configure-knative.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Post install script for configuring knative for dispatch
 
-: ${DISPATCH_NS:="dispatch-server"}
-: ${SERVING_NS:="knative-serving"}
+: ${DISPATCH_NAMESPACE:="dispatch"}
+: ${SERVING_NAMESPACE:="knative-serving"}
+: ${RELEASE_NAME:="knative-serving"}
 
 # Get the clusterIP address of the docker registry
-DOCKER_REG_IP=$(kubectl get service --namespace ${DISPATCH_NS} ${DISPATCH_NS}-docker-registry -o jsonpath="{.spec.clusterIP}")
+DOCKER_REG_IP=$(kubectl get service --namespace ${DISPATCH_NAMESPACE} ${RELEASE_NAME}-docker-registry -o jsonpath="{.spec.clusterIP}")
 # Update the knative serving controller to skip validation for the registry (it's insecure)
-kubectl -n $SERVING_NS get configmap config-controller -o yaml | sed 's/registriesSkippingTagResolving:\(.*\)/registriesSkippingTagResolving:\1,'${DOCKER_REG_IP}:5000'/g' > edited.yaml
+kubectl -n $SERVING_NAMESPACE get configmap config-controller -o yaml | sed 's/registriesSkippingTagResolving:\(.*\)/registriesSkippingTagResolving:\1,'${DOCKER_REG_IP}:5000'/g' > edited.yaml
 kubectl apply -f edited.yaml
 # Bounce the controller to take advantage of the new config
-POD_NAME=$(kubectl get pods --namespace $SERVING_NS -l "app=controller" -o jsonpath="{.items[0].metadata.name}")
-kubectl -n $SERVING_NS delete pod $POD_NAME
+POD_NAME=$(kubectl get pods --namespace $SERVING_NAMESPACE -l "app=controller" -o jsonpath="{.items[0].metadata.name}")
+kubectl -n $SERVING_NAMESPACE delete pod $POD_NAME
--- scripts/values.sh
+++ scripts/values.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 
+: ${DISPATCH_NAMESPACE:="dispatch"}
+: ${DISPATCH_SERVER_DOCKER_REPOSITORY:="vmware"}
 : ${VALUES_PATH:="values.yaml"}
 
 cat << EOF > $VALUES_PATH
 image:
   host: ${DISPATCH_SERVER_DOCKER_REPOSITORY}
   tag: ${TAG}
+registry:
+  url: http://${DISPATCH_NAMESPACE}-docker-registry:5000/
+  repository: ${DISPATCH_NAMESPACE}-docker-registry:5000
 storage:
   minio:
+    address: ${DISPATCH_NAMESPACE}-minio:9000
     username: ${MINIO_USERNAME}
     password: ${MINIO_PASSWORD}
 EOF
